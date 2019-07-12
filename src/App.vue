e904cb0a3e5cc01bb7d6869f477239b77152379b
wxf <wxf_mx@163.com>
2019-07-12 16:11:02 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-card class="box-card">
+    <el-input v-model="text" placeholder="请输入内容" @></el-input>
+    <!-- <div v-for="o in 4" :key="o" class="text item">
+      {{'列表内容 ' + o }}
+    </div>-->
+    <div class="todo_list">
+      <li class="text todo" v-for="(item, index) in todoList" :key="index">
+        {{item.text}}
+        <el-button type="text" icon="el-icon-delete" circle></el-button>
+      </li>
+    </div>
+    <el-button>全部</el-button> 
+    <el-button>已完成</el-button>
+    <el-button>未完成</el-button>
+  </el-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const todoList = [{
+  id: 1,
+  text: '学习vue',
+  completed: true
+}, {
+  id: 2,
+  text: '今天心情不错',
+  completed: false
+}];
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: '',
+      todoList
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.text {
+  font-size: 14px;
+}
+
+.todo {
+  padding: 10px 0;
+}
+
+.box-card {
+  width: 480px;
 }
 </style>
